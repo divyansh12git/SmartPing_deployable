2967f2b3ab05a2b97c89988a1a5383b28e48fc13
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-01-12 21:19:37 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-screen flex justify-center items-center bg-green-200">
+    <div className="text-4xl text-blue-950 ">This is the repo for Green City 
+      Contribute to the codedsdsd :)
+    </div>
     </div>
   );
 }

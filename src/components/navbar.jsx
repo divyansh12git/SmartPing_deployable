9ddf87c1716df3d326c4ccae226ef70406f0de5a
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-02-16 03:02:22 +0530

    linking done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,12 +6,12 @@ function NavBar() {
     <div className=' w-[70%] ml-auto'>
         <ul className='flex flex-row gap-10  ml-20 text-[#4285F4] items-center justify-between w-[80%]  '>
 
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Home</li>
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Chat</li>
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Health</li>
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Traffic</li>
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Image Analyzer</li>
-            <li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>About Us</li>
+            <a href='/'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Home</li></a>
+            <a href='/chat'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Chat</li></a>
+            <a href='/health'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Health</li></a>
+            <a href='/traffic'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Traffic</li></a>
+            <a href='/imageAnalyzer'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>Image Analyzer</li></a>
+            <a href='/aboutUs'><li className='hover:bg-gray-600 hover:text-white  px-4 py-1 rounded-full'>About Us</li></a>
             
         </ul>
     </div>

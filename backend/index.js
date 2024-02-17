bd82c8677ac3b322e3786cf5963db8be9b11d159
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-02-17 21:09:15 +0530

    chatbot backend integrated

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,42 @@ async function generateContentFromGemini() {
     const text = response.text();
     console.log(text);
   }
+  async function chatBot() {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    let chatHistory=[
+      {
+        role: "user",
+        parts: "Hi, I am your bestfriend. ",
+      },
+      {
+        role: "model",
+        parts: "Yes, I am your bestfriend, tell me anything about yourself.",
+      },
+    ]
+    const chat = model.startChat({
+      history: chatHistory,
+      generationConfig: {
+        maxOutputTokens: 500,
+      },
+    });
+    const msg = "short note on taj mahal ";
+
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
+   chatHistory.push(
+    {
+      role:"user",
+      parts:msg
+    },
+    {
+      role:"model",
+      parts:text
+    }
+)
+  console.log(chatHistory[3].parts);
+}
 
 
 

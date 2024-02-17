1ff3d8d1b393a4772c21110f012b1d2d802e4435
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-02-17 12:52:56 +0530

    gemini connected

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,52 @@
 import express from "express";
+import   {GoogleGenerativeAI} from "@google/generative-ai" ;
+import 'dotenv/config';
+
 
 const app=express();
 const PORT=4000;
+
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+
+// console.log(process.env.GEMINI_API);
+
+async function run() {
+    const generationConfig = {
+       
+        // maxOutputTokens: 200,
+        temperature: 0.9,
+        topP: 0.1,
+        topK: 16,
+      };
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro",generationConfig});
+  
+    const prompt ="a short note on india";
+    const { totalTokens } = await model.countTokens(prompt);
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+
+
+
+
+
+
 
 
 const data={
     response:"server",
     hi:"bro"
 }
-
 app.get('/',(req,res)=>{
     res.json(data);
 })
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT} `)
-})
+    console.log(`Server is running on ${PORT}`)
+});
